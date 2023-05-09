3fdf9a627d64bce20042109a82631ca0b6551a2f
Foolers00 <83927332+Foolers00@users.noreply.github.com>
2023-05-09 16:46:29 +0200

    added example in main.c

--- main.c
+++ main.c
@@ -6,6 +6,16 @@
 
 
 int main(){
-    init_random();
+    Skip_list slist;
+    init_skip_list(&slist, 10);
+    
+    int numbers[] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < 5; i++){
+        add_skip_list(&slist, numbers[i], numbers[i]);
+    }
+    print_skip_list(&slist);
+    remove_skip_list(&slist, 3);
+    print_skip_list(&slist);
+    free_skip_list(&slist);
     return 0;
 }
