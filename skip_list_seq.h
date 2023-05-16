b203c02500642c9919c516030591ced28e7489a3
Foolers00 <83927332+Foolers00@users.noreply.github.com>
2023-05-16 16:14:23 +0200

    Changed random function

--- skip_list_seq.h
+++ skip_list_seq.h
@@ -33,12 +33,15 @@
 #include <string.h>
 #endif
 
+
+#define FRACTION 1/2 
+
 typedef struct Node_seq{
     struct Node_seq** next;
     struct Node_seq** prev;
     int key;
     int value;
-    int level;
+    unsigned int level;
 }Node_seq;
 
 typedef struct Window_seq{
@@ -50,17 +53,17 @@ typedef struct Window_seq{
 typedef struct Skip_list_seq{
     Node_seq* header;
     Node_seq* tail;
-    int max_level;
+    unsigned int max_level;
 }Skip_list_seq;
 
 
 
 /* 
     Initializes skip list slist with tail and header,
-    where both have an array of lists next and prev t
-    hat are max_level long and point to each other 
+    where both have an array of lists next and prev 
+    that are max_level long and point to each other 
 */
-void init_skip_list_seq(Skip_list_seq* slist, int max_level);
+bool init_skip_list_seq(Skip_list_seq* slist, unsigned int max_level);
 
 /* 
     adds the value to the skip list slist with the key,
@@ -93,11 +96,11 @@ Node_seq* find_skip_list_seq(Skip_list_seq* slist, int key, Node_seq** proto);
     it returns the node before it would insert the node with key
     a w double must be also given over to increase the search time,
 */
-Node_seq* find_list_seq(Skip_list_seq* slist, int level, int key, Window_seq* w);
+Node_seq* find_list_seq(Skip_list_seq* slist, unsigned int level, int key, Window_seq* w);
 
 /* 
     must be called before using function random_level_generator,
-    sets the seed
+    sets the seed, is called in init_skip_list_l
 */
 void init_random_seq();
 
@@ -105,7 +108,7 @@ void init_random_seq();
     returns random level count, 
     probability: level 0 = 100%, level 1 = 50%, level 2 = 25% ...
 */
-int random_level_generator_seq(int max_level);
+int random_level_generator_seq(unsigned int max_level);
 
 /*
     frees up memory for node
