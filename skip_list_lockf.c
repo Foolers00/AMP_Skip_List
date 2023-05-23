3d3fcd1d2bff67fd4e1f4a81ddbc9bbb1b6ff398
thecapt <chris-hofmann@hotmail.com>
2023-05-23 17:23:29 +0200

    Add header for lock free list + some init functions

--- skip_list_lockf.c
+++ skip_list_lockf.c
@@ -1,3 +1,108 @@
 /////////////////////////////////////////////////////////////////////////////////////
 /* SKIP LIST LOCK FREE */
 /////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SKIP_LIST_LOCKF
+#define SKIP_LIST_LOCKF
+#include "skip_list_lockf.h"
+#endif
+
+bool init_skip_list_lfree(Skip_list_lfree* slist, int max_level){
+
+    Node_lfree* header = NULL;
+    Node_lfree* tail = NULL;
+
+    init_random_lfree(slist);
+
+    if(!init_node_lfree(&tail, INT_MAX, 0,
+                    max_level)){ return false; }
+
+    if(!init_node_lfree(&header, INT_MIN, 0,
+                    max_level)){ return false; }
+
+    #pragma omp parallel default(shared)
+    {
+        for(int i = 0; i <= max_level; i++){
+            header->nexts[i]->ref = tail;
+            tail->nexts[i]->ref = NULL;
+        }
+    }
+
+
+    slist->header = header;
+    slist->tail = tail;
+    slist->max_level = max_level;
+
+    return true;
+
+}
+
+bool init_node_lfree(Node_lfree** node, int key, int value, unsigned int level){
+
+    *node = (Node_lfree*)malloc(sizeof(Node_lfree));
+
+    if(!*node){
+        fprintf(stderr, "Malloc failed");
+        return false;
+    }
+
+    (*node)->level = level;
+    (*node)->key = key;
+    (*node)->value = value;
+
+    (*node)->nexts = (Atomic_markable_reference **)malloc((level + 1) * sizeof(Atomic_markable_reference *));
+
+    if(!(*node)->nexts){
+        fprintf(stderr, "Malloc failed");
+        return false;
+    }
+
+    omp_init_nest_lock(&(*node)->lock);
+    for (int l = 0; l <= level; l++) {
+        (*node)->nexts[l]->marked = false;
+    }
+    (*node)->fullylinked = false;
+
+    return true;
+
+}
+
+bool init_random_lfree(Skip_list_lfree* slist){
+
+    time_t t;
+    int id;
+
+    slist->random_seeds =
+            (unsigned int*)malloc(sizeof(unsigned int)*omp_get_num_threads());
+    if(!slist->random_seeds){
+        fprintf(stderr, "Malloc failed");
+        return false;
+    }
+
+    #pragma omp parallel private(t, id)
+    {
+        id = omp_get_thread_num();
+        slist->random_seeds[id] = (unsigned) time(&t) + id;
+    }
+
+    return true;
+
+
+}
+
+unsigned int random_level_generator_lfree(Skip_list_lfree* slist){
+
+    unsigned int level = 0;
+    unsigned int* seed = &slist->random_seeds[omp_get_thread_num()];
+    unsigned int max_level = slist->max_level;
+
+    double random_number = (double)rand_r(seed)/(double)RAND_MAX;
+
+    while(random_number < FRACTION && level < max_level){
+        level++;
+    }
+
+    return level;
+
+}
+
--- skip_list_lockf.h
+++ skip_list_lockf.h
@@ -1,3 +1,148 @@
 /////////////////////////////////////////////////////////////////////////////////////
 /* SKIP LIST LOCK FREE */
 /////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef STDLIB
+#define STDLIB
+#include <stdlib.h>
+#endif
+
+#ifndef STDIO
+#define STDIO
+#include <stdio.h>
+#endif
+
+#ifndef TIME
+#define TIME
+#include <time.h>
+#endif
+
+#ifndef BOOL
+#define BOOL
+#include <stdbool.h>
+#endif
+
+#ifndef LIMITS
+#define LIMITS
+#include <limits.h>
+#endif
+
+#ifndef STRING
+#define STRING
+#include <string.h>
+#endif
+
+#ifndef OMP
+#define OMP
+#include <omp.h>
+#endif
+
+#ifndef SKIP_LIST_SEQ
+#define SKIP_LIST_SEQ
+#include "skip_list_seq.h"
+#endif
+
+#define FRACTION (1.0/2.0)
+
+struct Atomic_markable_reference;
+
+typedef struct Node_lfree{
+    struct Atomic_markable_reference** nexts;
+    omp_nest_lock_t lock;
+    int key;
+    int value;
+    unsigned int level;
+    volatile bool fullylinked;
+}Node_lfree;
+
+typedef struct Skip_list_lfree{
+    Node_lfree* header;
+    Node_lfree* tail;
+    unsigned int max_level;
+    unsigned int* random_seeds;
+}Skip_list_lfree;
+
+
+typedef struct Window_l{
+    Node_lfree* pred;
+    Node_lfree* curr;
+}Window_l;
+
+typedef struct Atomic_markable_reference {
+    bool marked;
+    Node_lfree* ref;
+} Atomic_markable_reference;
+
+
+/*
+    Initializes skip list slist with tail and header,
+    where both have an array of lists next and prev
+    that are max_level long and point to each other
+*/
+bool init_skip_list_lfree(Skip_list_lfree* slist, int max_level);
+
+/*
+    Initializes node with key, value and level and allocates memory for
+    the node itselfs and the prevs and nexts arrays
+*/
+bool init_node_lfree(Node_lfree** node, int key, int value, unsigned int level);
+
+/*
+    must be called before using function random_level_generator,
+    sets the seeds for all threads, is called in init_skip_list_lfree
+*/
+bool init_random_lfree(Skip_list_lfree* slist);
+
+/*
+    returns random level count,
+    probability: level 0 = 100%, level 1 = 50%, level 2 = 25% ...
+*/
+unsigned int random_level_generator_lfree(Skip_list_lfree* slist);
+
+/*
+    Unlocks all the locks from prevs[0] upto prevs[level]
+*/
+void unlock_preds_lfree(Node_lfree* preds[], unsigned int level);
+
+// Melvin
+bool add_skip_list_lfree(Skip_list_lfree* slist, int key, int value);
+
+// Christopher
+bool remove_skip_list_lfree(Skip_list_lfree* slist, int key);
+
+// Thomas
+int find_skip_list_lfree(Skip_list_lfree* slist, int key, Node_lfree* preds[], Node_lfree* succs[]);
+
+// Thomas
+bool validate_skip_list_lfree(Window_l w, int l);
+
+/*
+    returns true if the key is in the skip list slist, otherwise false
+*/
+bool contains_skip_list_lfree(Skip_list_lfree* slist, int key);
+
+/*
+    frees up memory for node
+*/
+void free_node_lfree(Node_lfree* node);
+
+/*
+    frees up memory for skip list
+*/
+void free_skip_list_lfree(Skip_list_lfree* slist);
+
+/*
+    frees up memory for window
+*/
+void free_window_lfree(Window_l* w);
+
+/*
+    prints the skip list slist in the format (key, value)
+*/
+void print_skip_list_lfree(Skip_list_lfree* slist);
+
+/*
+    compares keys from slist_seq to slist_lfree, if not same error message
+    is printed out otherwise success message is printed out
+*/
+void compare_results_lfree(Skip_list_seq* slist_seq, Skip_list_lfree* slist_lfree);
